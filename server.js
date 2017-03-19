52304f9499367210e799a9a1d44c0d37f16ec68f
gilFuser <gil@gilfuser.net>
2017-03-19 21:02:23 +0100

    preparing for deploy

--- server.js
+++ server.js
@@ -1,27 +1,58 @@
+
+
+console.log("My Socket server is running");
+var socket = require('socket.io');
+
+
+// Based off of Shawn Van Every's Live Web
+// http://itp.nyu.edu/~sve204/liveweb_fall2013/week3.html
+
+// Using express: http://expressjs.com/
 var express = require('express');
+// Create the app
 var app = express();
-var server = app.listen(3000);
+
+// Set up the server
+// process.env.PORT is related to deploying on heroku
+var server = app.listen(process.env.PORT || 3000, listen);
+
+// This call back just tells us that the server has started
+function listen() {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Example app listening at http://' + host + ':' + port);
+}
 
 app.use(express.static('public'));
 
-console.log("My Socket server is running");
 
-var socket = require('socket.io');
+// WebSocket Portion
+// WebSockets work with the HTTP server
+var io = require('socket.io')(server);
 
-var io = socket(server);
-
+// Register a callback function to run when we have an individual connection
+// This is run for each individual user that connects
 io.sockets.on('connection', newConnection);
 
+// We are given a websocket object in our function
 function newConnection(socket) {
-  console.log( 'new connection: ' + socket.id);
 
- socket.on('mouse', mouseMsg);
+    console.log("We have a new client: " + socket.id);
+    // When this user emits, client side: socket.emit('otherevent',some data);
+    socket.on('mouse', mouseMsg );
 
- function mouseMsg(data) {
-   socket.broadcast.emit('mouse', data);
-   // ato send the msg to everyone, include back to ithe client that is sending
-   // io.sockets.emit('mouse, data');
-   console.log(data);
- }
-
+    function mouseMsg(data) {
+        // Data comes in as whatever was sent, including objects
+        console.log("Received: 'mouse' " + data.x + " " + data.y);
+        // Send it to all other clients
+        socket.broadcast.emit('mouse', data);
+        // This is a way to send to everyone including sender
+        // io.sockets.emit('message', "this goes to everyone");
+        console.log(data);
+    }
+  /*
+    socket.on('disconnect', function() {
+      console.log("Client has disconnected");
+      */
+    });
 }
