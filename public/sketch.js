bad231583a6813a43f3fbf5eac0c04e0afa8b912
gilFuser <gil@gilfuser.net>
2017-03-19 18:40:01 +0100

    preparing for deploy

---
+++ public/sketch.js
@@ -0,0 +1,32 @@
+var socket;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(51);
+
+  socket = io.connect('http://localhost:3000');
+  socket.on('mouse', newDrawing);
+}
+
+function newDrawing(data) {
+  noStroke();
+  fill(255, 0, 100);
+  ellipse(data.x, data.y, 20, 20);
+
+}
+
+function mouseDragged () {
+  console.log('Sending: ' + mouseX + ' , ' + int(mouseY));
+  var data = {
+    x: mouseX,
+    y: mouseY
+  }
+  socket.emit('mouse', data);
+
+    noStroke();
+    fill(255);
+    ellipse(data.x, data.y, 20, 20);
+}
+
+function draw() {
+}
--- server.js
+++ server.js
@@ -1,1 +1,27 @@
+var express = require('express');
+var app = express();
+var server = app.listen(3000);
+
+app.use(express.static('public'));
+
 console.log("My Socket server is running");
+
+var socket = require('socket.io');
+
+var io = socket(server);
+
+io.sockets.on('connection', newConnection);
+
+function newConnection(socket) {
+  console.log( 'new connection: ' + socket.id);
+
+ socket.on('mouse', mouseMsg);
+
+ function mouseMsg(data) {
+   socket.broadcast.emit('mouse', data);
+   // ato send the msg to everyone, include back to ithe client that is sending
+   // io.sockets.emit('mouse, data');
+   console.log(data);
+ }
+
+}
