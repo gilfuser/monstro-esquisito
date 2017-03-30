d2213a8c15ef606443ed52c8bb55afbcc81f38f4
gilFuser <gil@gilfuser.net>
2017-03-30 20:27:46 +0200

    add UI + make lines

--- public/sketch.js
+++ public/sketch.js
@@ -1,90 +1,121 @@
 var socket;
 
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-  background(51);
+var bgColor;
+var penColor;
+var colors;
+var lineThickness;
+var lineColor;
+var btnBg;
+var canv;
 
-  //app.set('port', (process.env.PORT || 5000));
+function setup() {
+  canv = createCanvas(windowWidth, windowHeight);
+  canv.parent('canvas');
+  colorMode(HSB);
+
+  bgColor = [random(0,360), random(0,25), random(10,20)];
+  background(bgColor[0], bgColor[1], bgColor[2]);
+  lineColor = [random(0,360), random(25,100), random(50,100), 100];
+  //lineThickness = 100;
+
   socket = io.connect();
   socket.on('mouse', newDrawing);
-}
+  socket.on('background', changeBGcolor);
 
+
+  brushColor = createDiv('#');
+  brushColor.style("background-color", color(lineColor[0],lineColor[1], lineColor[2], lineColor[3] ));
+  brushColor.style("color", color(0, 0 ));
+  brushColor.parent('brushcolor');
+  phue = createP('hue/tom');
+  phue.parent('tom');
+  hSlider = createSlider(0.0, 360.0, lineColor[0], 0);
+  hSlider.parent('tom');
+  psat = createP('saturation');
+  psat.parent('sat');
+  sSlider = createSlider(0.0, 100.0, lineColor[1], 0);
+  sSlider.parent('sat');
+  pbri = createP('brilho');
+  pbri.parent('bri');
+  bSlider = createSlider(0.0, 100.0, lineColor[2], 0);
+  bSlider.parent('bri');
+  ptransp = createP('transp.');
+  ptransp.parent('alp');
+  aSlider = createSlider(0.0, 100.0, lineColor[3], 0);
+  aSlider.parent('alp');
+  pthick = createP('thickness');
+  pthick.parent('thickness');
+  tSlider = createSlider(1, 40, 1, 0);
+  tSlider.parent('thickness');
+  // TO DO: Make a sample of the thickness
+  /*
+  sample = ellipse(0, 0, 20);
+  sample.parent('thickness');
+  */
+  /*
+  btnBg = createButton("bg-color");
+  btnBg.mouseClicked(changeBGcolor);
+  btnBg.parent('thickness');
+  */
+}
 function newDrawing(data) {
-  noStroke();
-  fill(255, 0, 100);
-  ellipse(data.x, data.y, 20, 20);
-
+  lineColor = [hSlider.value(), sSlider.value(), bSlider.value(), map(aSlider.value(), 0, 100, 0, 1)];
+  strokeWeight(data.thickness);
+  stroke(data.color[0], data.color[1], data.color[2], data.color[3]);
+  line(data.px, data.py, data.x, data.y );
 }
 
-function mouseDragged () {
-  console.log('Sending: ' + mouseX + ' , ' + int(mouseY));
+function touchStarted() {
+// do some stuff
+}
+
+function touchMoved() {
+  //update and send all data
   var data = {
+    px: pmouseX,
+    py: pmouseY,
     x: mouseX,
-    y: mouseY
+    y: mouseY,
+    color: lineColor,
+    thickness: lineThickness,
   }
   socket.emit('mouse', data);
 
-    noStroke();
-    fill(255);
-    ellipse(data.x, data.y, 20, 20);
+  lineThickness = tSlider.value();
+  lineColor = [hSlider.value(), sSlider.value(), bSlider.value(), map(aSlider.value(), 0, 100, 0, 1)];
+  strokeWeight(data.thickness);
+  stroke(data.color[0], data.color[1], data.color[2], data.color[3]);
+  line(data.px, data.py, data.x, data.y );
+  brushColor.style("background-color", color(data.color[0], data.color[1], data.color[2], data.color[3]));
+
+  // TO DO! OTHER TOOLS
+  //  noStroke();
+  //  fill(255);
+  //  ellipse(data.x, data.y, punktR, punktR);
+
+  //return false;
+}
+
+function changeBGcolor() {
+  var data = { color: lineColor };
+  //  console.log("bg has changed: " + data.color[0] );
+  bgColor = color(data.color[0], data.color[1], data.color[2], data.color[3]);
+  background(bgColor);
+  socket.emit('background', data);
+}
+
+function windowResized() {
+  canv.resizeCanvas(windowWidth, windowHeight);
+
 }
 
 function draw() {
+  //colorMode(RGB);
+  brushColor.style.backgroundColor = "hsb("+lineColor[0]+","+lineColor[1]+","+lineColor[2]+")";
+
+  //   colorMode(HSB);
+
 }
-
-
-// ITP Networked Media, Fall 2014
-// https://github.com/shiffman/itp-networked-media
-// Daniel Shiffman
 /*
-// Keep track of our socket connection
-var socket;
 
-function setup() {
-  createCanvas(400, 400);
-  background(0);
-  // Start a socket connection to the server
-  // Some day we would run this server somewhere else
-  socket = io.connect('http://localhost:3000');
-  // We make a named event called 'mouse' and write an
-  // anonymous callback function
-  socket.on('mouse',
-    // When we receive data
-    function(data) {
-      console.log("Got: " + data.x + " " + data.y);
-      // Draw a blue circle
-      fill(0,0,255);
-      noStroke();
-      ellipse(data.x, data.y, 20, 20);
-    }
-  );
-}
-
-function draw() {
-  // Nothing
-}
-
-function mouseDragged() {
-  // Draw some white circles
-  fill(255);
-  noStroke();
-  ellipse(mouseX,mouseY,20,20);
-  // Send the mouse coordinates
-  sendmouse(mouseX,mouseY);
-}
-
-// Function for sending to the socket
-function sendmouse(xpos, ypos) {
-  // We are sending!
-  console.log("sendmouse: " + xpos + " " + ypos);
-
-  // Make a little object with  and y
-  var data = {
-    x: xpos,
-    y: ypos
-  };
-
-  // Send that object to the socket
-  socket.emit('mouse',data);
-}
 */
--- server.js
+++ server.js
@@ -18,7 +18,7 @@ server.listen(PORT);
 console.log('Server started on port ' + PORT);
 
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/public/index.html');
+    res.sendFile('/public/index.html');
 });
 /*
 // This call back just tells us that the server has started
@@ -30,7 +30,7 @@ function listen() {
 */
 // WebSocket Portion
 // WebSockets work with the HTTP server
-var io = require('socket.io')(server);
+var io = require('socket.io').listen(server);
 
 // Register a callback function to run when we have an individual connection
 // This is run for each individual user that connects
@@ -45,15 +45,26 @@ io.sockets.on('connection',
       function(data) {
         // Data comes in as whatever was sent, including objects
         console.log("Received: 'mouse' " + data.x + " " + data.y);
-
         // Send it to all other clients
         socket.broadcast.emit('mouse', data);
-
         // This is a way to send to everyone including sender
         // io.sockets.emit('message', "this goes to everyone");
 
       }
     );
+    // When this user emits, client side: socket.emit('otherevent',some data);
+    socket.on('background',
+      function(data) {
+        // Data comes in as whatever was sent, including objects
+        console.log("bg has changed: " + data.color[0] );
+        // Send it to all other clients
+        socket.broadcast.emit('background', data);
+        // This is a way to send to everyone including sender
+        // io.sockets.emit('message', "this goes to everyone");
+
+      }
+    );
+
 
     socket.on('disconnect', function() {
       console.log("Client has disconnected");
